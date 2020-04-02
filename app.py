2aef1d60f6f573775d437d4a271749833ca5b6e9
Edward <edward1996@gmail.com>
2020-04-02 16:18:43 +0800

    finished exceptions section

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ charCountSorted = sorted(
 
 # return first item in descending dictionary
 print(charCountSorted[0])
-"""
+
 
 # Section: Exceptions
 
@@ -48,6 +48,7 @@ else:
 
 #  Cleaning up
 try:
+    # the with will close the file after execution"
     with open("app.py") as file:
         print("File opened.")
     age = int(input("Age :"))
@@ -56,3 +57,61 @@ except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age")
 else:
     print("No exceptions were thrown")
+
+
+# Raising exceptions
+
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+        #  similar to throw in java
+        #  raising exceptions is costly
+        #  can refer to documentation of python's exception
+    return 10/age
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
+
+"""
+
+#  Cost of Raising exceptions
+
+# from timeit import timeit
+
+# code1 = """
+# def calculate_xfactor(age):
+#     if age <= 0:
+#         raise ValueError("Age cannot be 0 or less.")
+#         #  similar to throw in java
+#         #  raising exceptions is costly
+#         #  can refer to documentation of python's exception
+#     return 10/age
+
+# try:
+#     calculate_xfactor(-1)
+# except ValueError as error:
+#     pass
+# """
+
+# code2 = """
+# def calculate_xfactor(age):
+#     if age <= 0:
+#         return None
+#         #  similar to throw in java
+#         #  raising exceptions is costly
+#         #  can refer to documentation of python's exception
+#     return 10/age
+
+# xfactor = calculate_xfactor(-1)
+# if xfactor == None:
+#     pass
+# """
+
+# print("first code", timeit(code1, number=10000))
+# print("second code", timeit(code2, number=10000))
+# #  returns the performance of running the code 10000 times
+# #  simple functions will not be affected but
+# #  still it is better to not raise exceptions
+# #  unless necessary
