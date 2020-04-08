8df4eba75e78fad64756d61eca3f31ef78961e55
Edward <edward1996@gmail.com>
2020-04-08 22:56:01 +0800

    removed tutorial material

--- py_stan_library/CLI_argument.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if len(sys.argv) == 1:
-    print("USAGE: python3 Main.py <password>")
-else:
-    password = sys.argv[1]
-    print("Password", password)
--- py_stan_library/random_values.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-print(random.random())
-print(random.choices([1, 2, 31, 2, 31, 32, 123, 1, 23, 13, 1], k=4))
-print("".join(random.choices(string.ascii_letters+string.digits, k=4)))
-
-# random.shuffle(list)
