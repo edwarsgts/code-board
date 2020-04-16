2fdf46219edd2a6666d74c7b3ee179959aa79bfd
Edward <edward1996@gmail.com>
2020-04-16 11:52:32 +0800

    added solutions

--- LC_30_Day.py
+++ LC_30_Day.py
@@ -428,7 +428,8 @@ def find_max_length(nums):
 """
 
 """
-Day 14 : Perform String Shifts
+Day 14: Perform String Shifts
+
 
 def string_shift(s, shift):
     # left shift +ve, right shift -ves
@@ -442,6 +443,106 @@ def string_shift(s, shift):
     # can take care of that
     count %= len(s)
     return s[count:] + s[:count]
+
+
+Day 15:
+
+
+def productExceptSelf_time_exceeded(nums):
+    output = []
+    i = 0
+    while (len(nums) > i):
+        result = 1
+        for j in range(1, len(nums)):
+            result *= nums[j]
+        output.append(result)
+        nums.append(nums.pop(0))
+        i += 1
+    return output
+
+
+def productExceptSelf(nums):
+    output = []
+    num_freq = collections.Counter(nums)
+    for num in nums:
+        result = 1
+        num_freq[num] -= 1
+        for k, v in num_freq.items():
+            result *= (k**v)
+        output.append(result)
+        num_freq[num] += 1
+    return output
+
+
+def productExceptSelf_algo1(nums):
+    # The length of the input array
+    length = len(nums)
+
+    # The left and right arrays as described in the algorithm
+    L, R, answer = [0]*length, [0]*length, [0]*length
+
+    # L[i] contains the product of all the elements to the left
+    # Note: for the element at index '0', there are no elements to the left,
+    # so the L[0] would be 1
+    L[0] = 1
+    for i in range(1, length):
+
+        # L[i - 1] already contains the product of elements to the left of 'i - 1'
+        # Simply multiplying it with nums[i - 1] would give the product of all
+        # elements to the left of index 'i'
+        L[i] = nums[i - 1] * L[i - 1]
+
+    # R[i] contains the product of all the elements to the right
+    # Note: for the element at index 'length - 1', there are no elements to the right,
+    # so the R[length - 1] would be 1
+    R[length - 1] = 1
+    for i in reversed(range(length - 1)):
+
+        # R[i + 1] already contains the product of elements to the right of 'i + 1'
+        # Simply multiplying it with nums[i + 1] would give the product of all
+        # elements to the right of index 'i'
+        R[i] = nums[i + 1] * R[i + 1]
+
+    # Constructing the answer array
+    for i in range(length):
+        # For the first element, R[i] would be product except self
+        # For the last element of the array, product except self would be L[i]
+        # Else, multiple product of all elements to the left and to the right
+        answer[i] = L[i] * R[i]
+
+    return answer
+
+
+def productExceptSelf_algo2(nums):
+    # The length of the input array
+    length = len(nums)
+
+    # The answer array to be returned
+    answer = [0]*length
+
+    # answer[i] contains the product of all the elements to the left
+    # Note: for the element at index '0', there are no elements to the left,
+    # so the answer[0] would be 1
+    answer[0] = 1
+    for i in range(1, length):
+
+        # answer[i - 1] already contains the product of elements to the left of 'i - 1'
+        # Simply multiplying it with nums[i - 1] would give the product of all
+        # elements to the left of index 'i'
+        answer[i] = nums[i - 1] * answer[i - 1]
+
+    # R contains the product of all the elements to the right
+    # Note: for the element at index 'length - 1', there are no elements to the right,
+    # so the R would be 1
+    R = 1
+    for i in reversed(range(length)):
+
+        # For the index 'i', R would contain the
+        # product of all elements to the right. We update R accordingly
+        answer[i] = answer[i] * R
+        R *= nums[i]
+
+    return answer
 
 
 """
