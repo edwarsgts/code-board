bcba5ee1b7037e200f99485b547374fef8dd0d34
Edward <edward1996@gmail.com>
2020-04-18 13:16:48 +0800

    minor fix

--- GFG_solutions/Diameter of BinTree.py
+++ GFG_solutions/Diameter of BinTree.py
@@ -12,10 +12,10 @@ class Node:
         self.right = None
 
 
-""" 
-The function Compute the "height" of a tree. Height is the 
-number f nodes along the longest path from the root node 
-down to the farthest leaf node. 
+"""
+The function Compute the "height" of a tree. Height is the
+number f nodes along the longest path from the root node
+down to the farthest leaf node.
 """
 
 
@@ -55,13 +55,13 @@ def diameter(root):
 
 # Driver program to test above functions
 
-""" 
-Constructed binary tree is 
-			1 
-		  / \ 
-		2	 3 
-	  / \ 
-	4	 5 
+"""
+Constructed binary tree is
+			1
+		  / \
+		2	 3
+	  / \
+	4	 5
 """
 
 root = Node(1)
@@ -69,6 +69,6 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-print "Diameter of given binary tree is %d" % (diameter(root))
+print("Diameter of given binary tree is %d" % (diameter(root)))
 
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007)
