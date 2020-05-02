ec35507d74c6e204a815b5dded281a2bdf52dd54
Edward <edward1996@gmail.com>
2020-05-02 16:26:32 +0800

    Added daily solution

---
+++ LC_May_Challenge.py
@@ -0,0 +1,37 @@
+#  May LeetCode Challenge Archives
+
+# Day 1 : First Bad Version
+
+
+def first_bad_version_0(n):
+    # Using Binary Search
+    left = 1
+    right = n
+    while left < right:
+        mid = (left + right) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+
+def first_bad_version_1(n):
+    import bisect
+    # using Bisect
+    self.__getitem__ = isBadVersion
+    return bisect.bisect(self, True, 1, n)
+
+
+def isBadVersion():
+    # function provided by question
+    return
+
+# Day 2 : Jewels in Stones
+
+
+def num_jewels_in_stones(J, S):
+    result = 0
+    for jewel in J:
+        result += S.count(jewel)
+    return result
