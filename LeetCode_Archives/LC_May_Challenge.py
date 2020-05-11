815847f7a0aed074f6ffd0557b2a75515cec85de
Edward <edward1996@gmail.com>
2020-05-11 21:23:08 +0800

    Added daily solution

--- LeetCode_Archives/LC_May_Challenge.py
+++ LeetCode_Archives/LC_May_Challenge.py
@@ -124,3 +124,27 @@ def is_perfect_square(num):
     while r*r > num:
         r = (r + num/r) // 2
     return r*r == num
+
+# Day 10: Find town Judge
+
+
+def find_judge(N, trust):
+    """
+    In a town, there are N people labelled from 1 to N.  There is a rumor that one 
+    of these people is secretly the town judge.
+    If the town judge exists, then:
+    1. The town judge trusts nobody.
+    2. Everybody (except for the town judge) trusts the town judge.
+    3. There is exactly one person that satisfies properties 1 and 2.
+    You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+    If the town judge exists and can be identified, 
+    return the label of the town judge.  Otherwise, return -1.
+    """
+    count = [0] * (N+1)
+    for i, j in trust:
+        count[i] -= 1
+        count[j] += 1
+    for i in range(1, N+1):
+        if count[i] == N - 1:
+            return i
+    return -1
