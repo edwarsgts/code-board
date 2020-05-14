4e4ef73252f755f7ae208137bd98e74f6f41543a
Edward <edward1996@gmail.com>
2020-05-14 20:24:36 +0800

    Added Daily Solution

--- LeetCode_Archives/LC_May_Challenge.py
+++ LeetCode_Archives/LC_May_Challenge.py
@@ -148,3 +148,218 @@ def find_judge(N, trust):
         if count[i] == N - 1:
             return i
     return -1
+
+# Day 11: Flood fill
+
+
+def flood_fill(image, sr, sc, newColor):
+    if image[sr][sc] == newColor:
+        return
+
+    original_color = image[sr][sc]
+
+    def helper(image, sr, sc, newColor):
+        sr_not_valid = sr < 0 or sr >= len(image)
+        sc_not_valid = sc < 0 or sc >= len(image[0])
+        # base condition , if out of grid/not original color
+        if sr_not_valid or sc_not_valid or image[sr][sc] != original_color:
+            return
+        image[sr][sc] = newColor
+        helper(image, sr-1, sc, newColor)
+        helper(image, sr+1, sc, newColor)
+        helper(image, sr, sc-1, newColor)
+        helper(image, sr, sc+1, newColor)
+    helper(image, sr, sc, newColor)
+    return image
+
+
+def flood_fill_best_time(image, sr, sc, newColor):
+    if len(image) == 0:
+        return []
+
+    X, Y = len(image), len(image[0])
+    color = image[sr][sc]
+
+    if color == newColor:
+        return image
+
+    frontier_stack = [(sr, sc)]
+    while len(frontier_stack) > 0:
+        point = frontier_stack.pop()
+        x, y = point[0], point[1]
+        image[x][y] = newColor
+
+        candidates = ((x + 1, y),
+                      (x - 1, y),
+                      (x, y + 1),
+                      (x, y - 1),)
+        for cand in candidates:
+            x, y = cand[0], cand[1]
+            if x >= X or x < 0 or y >= Y or y < 0:
+                continue
+            if image[x][y] == color:
+                frontier_stack.append(cand)
+
+    return image
+
+# Day 12: Single Element in a Sorted Array
+
+
+def single_non_duplicate(nums):
+    import collections
+    # num_freq = collections.OrderedDict(collections.Counter(nums))
+    num_freq = collections.Counter(nums)
+    for k, v in num_freq.items():
+        if v == 1:
+            return k
+
+
+test_input = [1, 1, 2, 3, 3, 4, 4, 8, 8]
+expected_output = 2
+
+x = single_non_duplicate(test_input)
+print(x)
+
+
+def single_non_duplicate_60ms(nums):
+    """
+    1  1  4  4  5  5  6  8  8
+               mid .
+    1  1  4  5  5  6  6  8  8  9
+             .    
+    """
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if mid < len(nums) - 1 and nums[mid] == nums[mid + 1]:
+            halves_are_even = right - mid - 1
+            if halves_are_even % 2 == 1:
+                left = mid + 2
+            else:
+                right = mid - 1
+        elif mid > 0 and nums[mid] == nums[mid - 1]:
+            halves_are_even = right - mid
+            if halves_are_even % 2 == 1:
+                left = mid + 1
+            else:
+                right = mid - 2
+        else:
+            return nums[mid]
+    return -1
+
+# Day 13: Remove K Digits
+
+
+def removeKdigits(num: str, k: int) -> str:
+    out = []
+    for d in num:
+        while k and out and out[-1] > d:
+            out.pop()
+            k -= 1
+        out.append(d)
+    return ''.join(out[:-k or None]).lstrip('0') or '0'
+
+# Day 14: Implement Trie
+
+
+class Trie_116ms:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        t = self.trie
+        for c in word:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+        # Used to signify end of word
+        t['#'] = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        t = self.trie
+        for c in word:
+            if c not in t:
+                return False
+            t = t[c]
+        return '#' in t
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        t = self.trie
+        for c in prefix:
+            if c not in t:
+                return False
+            t = t[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+class Trie:
+    # 196ms
+    class TrieNode:
+        def __init__(self):
+            self.word = False
+            self.children = {}
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = self.TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                node.children[i] = self.TrieNode()
+            node = node.children[i]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for i in prefix:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
