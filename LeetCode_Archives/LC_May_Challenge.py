54e2daf171c7a4be44cfb561ae3e8227ff0ce943
Edward <edward1996@gmail.com>
2020-05-15 20:44:04 +0800

    Added Daily Solution

--- LeetCode_Archives/LC_May_Challenge.py
+++ LeetCode_Archives/LC_May_Challenge.py
@@ -363,3 +363,16 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+# Day 15: Maximum sum circular subarray
+
+def maxSubarraySumCircular(self, A: List[int]) -> int:
+    total, maxSum, curMax, minSum, curMin = 0, - \
+        float('inf'), 0, float('inf'), 0
+    for a in A:
+        curMax = max(curMax + a, a)
+        maxSum = max(maxSum, curMax)
+        curMin = min(curMin + a, a)
+        minSum = min(minSum, curMin)
+        total += a
+    return max(maxSum, total - minSum) if maxSum > 0 else maxSum
